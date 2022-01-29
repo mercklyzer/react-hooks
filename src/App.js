1923e22156abe4befa82c32919ef878521384c19
mercklyzer <mercklyzer@gmail.com>
2022-01-30 01:53:35 +0800

    Learn useMemo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { Hello } from "./Hello";
-import { Square } from "./Square";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import {useFetch} from './useFetch'
+
 
 const App = () => {
-  const [count,setCount] = useState(0)
-  const favNums = [7, 21, 27]
+  const [count, setCount] = useState(0)
 
-  const increment = useCallback((n) => {
-    // setCount(c => c + 1)
-    setCount(c => c + n)
-  }, [])
+  const {data} = useFetch('https://type.fit/api/quotes')
+
+  //alternatively just move this function outside App and remove this from the useMemo dependency
+  const computeLongestWord = useCallback((arr) => {
+    if(!arr){
+      return []
+    }
+
+    console.log("computing");
+
+    let longestWord =''
+
+
+    JSON.parse(data).map((quote) => quote.text).forEach(sentence => sentence.split(' ').forEach(word => {
+      if(word.length > longestWord.length){
+        longestWord = word
+      }
+    }))
+    
+    return longestWord
+  }, []) //if you have no dependency here, just move the function outside
+
+  const longestWord = useMemo(() => computeLongestWord(data), [data, computeLongestWord])
+
+
 
   return (
     <div>
-      <Hello increment={increment}/>
-      <div>count: {count}</div>
-      {favNums.map((n) => {
-        return <Square increment={increment} n={n} key={n}/>
-      })}
+      <div>Count: {count}</div>
+      <button onClick={() => setCount(count + 1)}>increment</button>
+      <div>{longestWord}</div>
     </div>
   );
 };
